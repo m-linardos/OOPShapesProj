df6535c7f693eae2492f2579c72cb8b9898fbc02
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-30 12:07:15 -0400

    Add project files.

---
+++ OOP-Shapes/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Shapes {
+    public class Quad {
+
+        // PROPERTIES
+        private int side1 { get; set; }
+        private int side2 { get; set; }
+        private int side3 { get; set; }
+        private int side4 { get; set; }
+        //private int perimeter = side1 + side2 + side3 + side4;
+
+        //public int GetPerimeter() {       // public - called outside of class  / new popular naming convention - noun defining method / -->  Perimeter()- gets value; Perimeter(intp)- ets the value
+        //    int perimeter = side1 + side2 + side3 + side4;      // prof. devs tend not to create variables when not needed
+        //    return perimeter;
+
+        public int GetPerimeter() {       // public - called outside of class  / new popular naming convention - noun defining method / -->  Perimeter()- gets value; Perimeter(intp)- ets the value
+            return side1 + side2 + side3 + side4;      // prof. devs tend not to create variables when not needed
+        }
+
+        public Quad(int side_a, int side_b, int side_c, int side_d) {             // constructors have no return type / has to have same name as class
+            this.side1 = side_a; // taking value passed in first interger & storing in side1
+            this.side2 = side_b; // constructors only used with 'new' (it is a special method)
+            this.side3 = side_c;
+            this.side4 = side_d;
+        }
+    }
+}
---
+++ OOP-Shapes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP_Shapes {
+    class Program {
+        static void Main(string[] args) {
+            
+            var quad1 = new Quad();
+            //quad1.side1 = 5;          /// not needed if you initialize it in the constructor
+            //quad1.side2 = 7;
+            //quad1.side3 = 13;
+            //quad1.side4 = 6;
+            var perimeter = quad1.GetPerimeter();
+
+            
+
+            Console.WriteLine($"The perimeter is {perimeter}");
+        }
+    }
+}
